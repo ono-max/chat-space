f5f59bdb8b1c4ed29f9e6a2bcf95da5eb3db5299
ono-max <nono19@students.desu.edu>
2020-01-17 21:05:53 +0900

    create log-out and flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-
-  get 'welcome/index'
+  # get 'messages/index'
+  # get 'welcome/index'
   root "messages#index"
+  resources :users, only: [:edit, :update]
 end
