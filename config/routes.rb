88b5321358539d4c2fdcab2c9cb585654a1540fc
ono-max <nono19@students.desu.edu>
2020-01-18 13:28:06 +0900

    arrange view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
   # get 'welcome/index'
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update, :index]
 end
